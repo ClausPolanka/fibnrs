5614023ed16aba38a40a4e4c83663ed355074f78
claus.polanka@chello.at <claus.polanka@chello.at>
2015-03-07 14:17:49 +0100

    Passed all acceptance tests.

--- fibnrs.test/FibunacciNumbersTest.cs
+++ fibnrs.test/FibunacciNumbersTest.cs
@@ -16,6 +16,10 @@ namespace fibnrs.test
         [TestCase(4, 3)]
         [TestCase(5, 5)]
         [TestCase(6, 8)]
+        [TestCase(19, 4181)]
+        [TestCase(28, 317811)]
+        [TestCase(36, 14930352)]
+        [TestCase(38, 39088169)]
         public void Get_Fibunacci_number_for_given_index(int index, int expectedFibunacciNumber)
         {
             var sut = new FibunacciNumbers();
